167400957c354207fdbbe19fb52f1888f8ced249
Salman <salman@chotu.com>
2025-08-02 13:01:04 +0530

    task done

--- lib/utils/responsive_utils.dart
+++ lib/utils/responsive_utils.dart
@@ -24,31 +24,7 @@ class ResponsiveUtils {
   }
 
   static Widget getResponsiveContainer(BuildContext context, Widget child) {
-    if (isWeb(context)) {
-      return Scaffold(
-        backgroundColor: Colors.grey[50], // Light background for web
-        body: Center(
-          child: Container(
-            width: getMaxContentWidth(context),
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 10,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: child,
-            ),
-          ),
-        ),
-      );
-    }
+    // Return child directly for web - no portrait-like container
     return child;
   }
 }
